25fe2405ebdb70b62398af977b98def22b6a7901
Andriy-Kolokolov <koloandre22@gmail.com>
2023-05-29 13:41:49 +0200

    Add components

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import AppHeader from "./AppHeader.vue";
+import AppMain from "./AppMain.vue";
+import AppFooter from "./AppFooter.vue";
+import MainSection from "./MainSection.vue"
+
+export {
+    AppHeader,
+    AppMain,
+    AppFooter,
+    MainSection
+}
