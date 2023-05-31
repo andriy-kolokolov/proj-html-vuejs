8bb59083e9c52cebb309db7d0df70ab70be00cdc
Andriy-Kolokolov <koloandre22@gmail.com>
2023-05-31 15:35:39 +0200

    add sticky header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,8 +4,10 @@
       <header-timer/>
     </div>
   </div>
-  <div class="container-fluid nav-wrapper ">
+  <div class="container-fluid nav-wrapper sticky-top ">
     <header-navigation class="header-navigation"/>
+  </div>
+  <div class="container-fluid">
     <header-hero/>
   </div>
 </template>
@@ -14,14 +16,39 @@
 import {HeaderTimer} from "./UI/index.js"
 import HeaderNavigation from "./HeaderNavigation.vue";
 import HeaderHero from "./HeaderHero.vue";
+
 export default {
-  components: {HeaderNavigation, HeaderTimer, HeaderHero}
+  components: {HeaderNavigation, HeaderTimer, HeaderHero},
+  methods: {
+    handleScroll() {
+      const header = document.querySelector(".nav-wrapper")
+      if (window.pageYOffset > 50) {
+        header.classList.add("active");
+      } else {
+        header.classList.remove("active");
+      }
+    }
+  },
+  mounted() {
+    window.addEventListener("scroll", this.handleScroll)
+  },
+  destroyed() {
+    window.removeEventListener("scroll", this.handleScroll);
+  },
+
 }
 </script>
 
 <style lang="sass" scoped>
 
 @use '../assets/main' as *
+
+.nav-wrapper
+  transition: .7s
+
+  &.active
+    background-color: rgba(255, 255, 255, 0.9)
+    padding: 0
 
 .timer-wrapper
   background-color: $palette-light
